4d6236dc9d35cc6939754b0f1205e626a174534c
majkelmichel <51023578+majkelmichel@users.noreply.github.com>
2020-06-09 16:27:57 +0200

    dodana pierwsza podstrona

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 var menuShown = false;
+var subMenuShown = false;
 
 function showMenu() {
 	if (menuShown) {
 		document.querySelector("menu").style.display = "none";
+		subMenuShown = false;
 		menuShown = false;
 	}
 	else {
 		document.querySelector("menu").style.display = "grid";
+		document.getElementById("submenu").style.display = "none";
 		menuShown = true;
+	}
+}
+
+function showSubMenu() {
+	if (subMenuShown) {
+		document.getElementById("submenu").style.display = "none";
+		subMenuShown = false;
+	}
+	else {
+		document.getElementById("submenu").style.display = "grid";
+		document.querySelector("menu").style.display = "none";
+		menuShown = false;
+		subMenuShown = true;
 	}
 }
